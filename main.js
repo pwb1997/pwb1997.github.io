3c8f0535049dd6eca6c39f507a2b5350f2239c09
pwb1997 <pwb1997@hotmail.com>
2018-03-29 15:54:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ Alternatively, 'random' command will take you to a random directory.
     }
 
     function random() {
-        magicNum = Math.floor(Math.random() * posts.length);
+        var magicNum = Math.floor(Math.random() * posts.length);
         terminal.append("Opening '" + posts[magicNum] + "' ...\n");
         window.location.href = "posts/" + posts[magicNum].url + ".html";
     }
